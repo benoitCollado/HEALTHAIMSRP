cab8d0ab1c5194337e66b8dcd6258f090959b178
DzmitryiKorjik <dmardovitch@gmail.com>
2026-04-28 14:58:39 +0200

    migration BDD sur Neon, ajouter lien connection sur la fichier .env

--- database/init/02_insert_test_data.sql
+++ database/init/02_insert_test_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO utilisateurs (age, sexe, taille_cm, poids_kg, niveau_activite, type_abonnement, date_inscription)
-VALUES (30, 'H', 175, 78, 'modere', 'freemium', CURRENT_DATE);
+INSERT INTO utilisateurs (username, password_hash, age, sexe, taille_cm, poids_kg, niveau_activite, type_abonnement, date_inscription)
+VALUES ('testuser', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW', 30, 'H', 175, 78, 2, 1, CURRENT_DATE);
--- database/init/03_add_nutriments_aliment.sql
+++
@@ -1,6 +0,0 @@
--- Migration : ajout sucres_g et acides_gras_satures_g à la table aliment
--- Exécuté automatiquement au premier init. Pour une BDD existante :
---   docker cp database/init/03_add_nutriments_aliment.sql postgres_health:/tmp/
---   docker exec postgres_health psql -U healthuser -d healthdb -f /tmp/03_add_nutriments_aliment.sql
-ALTER TABLE aliment ADD COLUMN IF NOT EXISTS sucres_g DECIMAL(6,2);
-ALTER TABLE aliment ADD COLUMN IF NOT EXISTS acides_gras_satures_g DECIMAL(6,2);
